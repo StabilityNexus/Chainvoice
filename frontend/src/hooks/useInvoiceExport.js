11c62dda14f4a2fa08788dee53ca6917c6c917fa
Atharva0506 <atharvan.coder@gmail.com>
2026-07-10 11:43:12 +0530

    fix: remove unused useState import

--- frontend/src/hooks/useInvoiceExport.js
+++ frontend/src/hooks/useInvoiceExport.js
@@ -1,4 +1,4 @@
-import { useState, useCallback } from "react";
+import { useCallback } from "react";
 import toast from "react-hot-toast";
 import { downloadInvoiceCSV } from "@/utils/generateInvoiceCSV";
 import { downloadInvoiceJSON } from "@/utils/generateInvoiceJSON";
